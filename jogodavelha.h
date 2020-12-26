44c942ba7a156c7a676410dd6712d568786a0f44
Emily <69693425+ShiroiB@users.noreply.github.com>
2020-12-26 20:29:57 -0300

    Create jogodavelha.h

---
+++ jogodavelha.h
@@ -0,0 +1,25 @@
+#ifndef _Contatos_H_
+#define _Contatos_H_
+
+
+
+void limpar();
+void pausar();
+void comecar(char a[][3] );
+void mostrar(char a[][3]);
+void lerString(char var[], int limite);
+void lernome(char var[], int tam);
+int  comp(char a[][3]);
+void nome(char no[20],char no2[20]);
+int again();
+void t2(char o[], char b[20], char b2[20], int x);
+void t(char o[], char x[20]);
+void jogo2(char no[20],char no2[20], char b[][3],int x);
+int  loop();
+void exibir(char no[20],char no2[20]);
+void escolha (char no[20],char no2[20],char b[][3]);
+
+#endif
+
+
+
